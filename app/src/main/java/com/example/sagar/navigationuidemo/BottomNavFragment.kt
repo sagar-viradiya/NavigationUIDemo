0a2360f95adba3db9dc6c2a6de7236fcbc485465
Sagar Viradiya <sagarviradiya4444@gmail.com>
2018-07-03 13:39:41 +0530

    Added 2 fragment
    - Created 2 top level fragments BottomNavFragment and InfoFragment

---
+++ app/src/main/java/com/example/sagar/navigationuidemo/BottomNavFragment.kt
@@ -0,0 +1,35 @@
+package com.example.sagar.navigationuidemo
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BottomNavFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class BottomNavFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_nav, container, false)
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment.
+         *
+         * @return A new instance of fragment BottomNavFragment.
+         */
+        @JvmStatic
+        fun newInstance() = BottomNavFragment()
+    }
+}
---
+++ app/src/main/java/com/example/sagar/navigationuidemo/InfoFragment.kt
@@ -0,0 +1,35 @@
+package com.example.sagar.navigationuidemo
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InfoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class InfoFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment InfoFragment.
+         */
+        @JvmStatic
+        fun newInstance() = InfoFragment()
+    }
+}
