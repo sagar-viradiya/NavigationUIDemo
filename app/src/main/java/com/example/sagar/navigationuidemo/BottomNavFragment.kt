9bf72c73b09f2635d347480b3a9e8e9c9027dce1
Sagar Viradiya <sagarviradiya4444@gmail.com>
2018-07-03 13:49:26 +0530

    Added Navigation component dependencies and revert from androidx to support lib
    - Reverted androidx dependencies for appcompat, material components and support v4 to support lib
    - Refactored fragments and activity to use support lib dependencies

--- app/src/main/java/com/example/sagar/navigationuidemo/BottomNavFragment.kt
+++ app/src/main/java/com/example/sagar/navigationuidemo/BottomNavFragment.kt
@@ -2,7 +2,7 @@ package com.example.sagar.navigationuidemo
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/example/sagar/navigationuidemo/MainActivity.kt
+++ app/src/main/java/com/example/sagar/navigationuidemo/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.sagar.navigationuidemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
