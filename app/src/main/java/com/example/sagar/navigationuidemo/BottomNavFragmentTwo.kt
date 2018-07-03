5616e5a6330cb43ec08b0976f734599d01a7bb86
Sagar Viradiya <sagarviradiya4444@gmail.com>
2018-07-03 14:39:00 +0530

    Fragments for bottom navigation
    - Added 3 fragments for BottomNavigationView

---
+++ app/src/main/java/com/example/sagar/navigationuidemo/BottomNavFragmentTwo.kt
@@ -0,0 +1,35 @@
+package com.example.sagar.navigationuidemo
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BottomNavFragmentTwo.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class BottomNavFragmentTwo : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_nav_fragment_two, container, false)
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment BottomNavFragmentTwo.
+         */
+        @JvmStatic
+        fun newInstance() = BottomNavFragmentTwo()
+    }
+}
