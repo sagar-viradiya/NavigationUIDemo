30c2b9e448892850f677331f9b18f3caaf046122
Sagar Viradiya <sagarviradiya4444@gmail.com>
2018-07-03 14:19:15 +0530

    Hooking up NavigationView to nav graph
    - Added logic in main activity to hook NavigationView to Nav graph

--- app/src/main/java/com/example/sagar/navigationuidemo/MainActivity.kt
+++ app/src/main/java/com/example/sagar/navigationuidemo/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.sagar.navigationuidemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.mainNavFragment)
+
+        // Set up ActionBar
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        // Set up navigation menu
+        navigationView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Look for an action or destination matching the menu item ID and navigate there is found.
+        // Otherwise, bubble up to the parent.
+
+        return NavigationUI.onNavDestinationSelected(item,
+                Navigation.findNavController(this, R.id.mainNavFragment))
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(drawerLayout,
+                Navigation.findNavController(this, R.id.mainNavFragment))
     }
 }
